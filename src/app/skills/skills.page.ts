e070b203c74305b708f08b260defdba2bdca9c4b
yxd3549 <yxd3549@rit.edu>
2018-12-10 14:34:36 -0500

    Finished skill form

--- src/app/skills/skills.page.ts
+++ src/app/skills/skills.page.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import {Validators, FormBuilder, FormGroup } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {Validators, FormBuilder, FormGroup} from '@angular/forms';
+import {HTTP} from '@ionic-native/http/ngx';
 
 @Component({
-  selector: 'app-skills',
-  templateUrl: './skills.page.html',
-  styleUrls: ['./skills.page.scss'],
+    selector: 'app-skills',
+    templateUrl: './skills.page.html',
+    styleUrls: ['./skills.page.scss'],
 })
 export class SkillsPage implements OnInit {
 
     private skill_search_form: FormGroup;
+    private skills = ['python', 'java', 'oracle', 'testing'];
 
-  constructor() { }
+    constructor(private http: HTTP, private formBuilder: FormBuilder) {
+        this.skill_search_form = this.formBuilder.group({
+            skill: ['', Validators.required],
+        });
+    }
 
-  ngOnInit() {
-  }
+    logForm(){
+        console.log(this.skill_search_form.value);
+    }
+
+    ngOnInit() {
+    }
 
 }
